3d8d252947d4d86e07c8c42efd94cd06911f353a
KokhanVika <kohvika198@gmail.com>
2021-12-21 22:56:40 +0200

    done 1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryConteiner = document.querySelector('.gallery');
+
+const photoMarkup = createGallery(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('beforeend', photoMarkup);
+
+
+function createGallery(galleryItems) {
+return galleryItems.map(({preview, original, description}) => 
+  { return `
+  <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  </div>
+  `
+  }).join('')
+};
+
+let galleryNew = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: "250ms"});
+
+galleryNew.on('show.simplelightbox', function () {
+  console.log("Не требует прослушивание кликов")
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const key = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimePlayer, 1000));
+
+function onTimePlayer(event) {
+localStorage.setItem(key, `${event.seconds}`);
+}
+
+const savedTime = localStorage.getItem(key);
+if(savedTime){
+  player.setCurrentTime(savedTime);
+}
